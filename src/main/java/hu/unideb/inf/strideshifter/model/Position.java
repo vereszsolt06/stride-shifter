e21c491dbe57e69ad0d0f7a8897b8628c0b9aefb
Zsolt Veres <vereszsolt06@gmail.com>
2026-06-19 02:34:09 +0200

    Add move method to Position record

--- src/main/java/hu/unideb/inf/strideshifter/model/Position.java
+++ src/main/java/hu/unideb/inf/strideshifter/model/Position.java
@@ -7,4 +7,19 @@ package hu.unideb.inf.strideshifter.model;
  * @param col the column index (0-based, left to right)
  */
 public record Position(int row, int col) {
+
+    /**
+     * Calculates a new position obtained by moving from this position
+     * in the specified direction by a given distance.
+     *
+     * @param direction the direction of the movement
+     * @param distance the number of cells to move
+     * @return the new position after the movement
+     */
+    public Position move(Direction direction, int distance){
+        return new Position(
+                this.row+direction.getRowChange()*distance,
+                this.col+direction.getColChange()*distance
+        );
+    }
 }
